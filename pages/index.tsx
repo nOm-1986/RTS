fffc71287832b27929864543ad82fc50d99c36ac
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-25 10:45:59 -0500

    Primitives types in State

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import type { NextPage } from 'next';
 import Image from 'next/image'
 import { RandomFox } from '@/components/RandomFox';
@@ -5,12 +6,24 @@ import { RandomFox } from '@/components/RandomFox';
 const random = ():number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Hello platzi!
       </h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
+      {images.map((image, index)=>(
+        <div key={index} className="p-4">
+          <RandomFox image={image}/>
+        </div>
+      ))}
+
     </main>
   )
 }
