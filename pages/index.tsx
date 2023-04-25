4eb0a1105f1282bd8d40f15d8818f06f9784e9d7
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-25 11:03:39 -0500

    Using types on Hook State

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,17 @@ import Image from 'next/image'
 import { RandomFox } from '@/components/RandomFox';
 
 const random = ():number => Math.floor(Math.random() * 123) + 1;
+const generateId = () => Math.random().toString(36).substring(2, 9);
+
+
+type ImageItem = {id: string, url: string};
 
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
   ]);
 
   return (
@@ -18,9 +22,9 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello platzi!
       </h1>
-      {images.map((image, index)=>(
-        <div key={index} className="p-4">
-          <RandomFox image={image}/>
+      {images.map((image)=>(
+        <div key={image.id} className="p-4">
+          <RandomFox image={image.url}/>
         </div>
       ))}
 
