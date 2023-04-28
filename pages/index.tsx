5e190308794526124cb7c70d5ccf0da953b615bd
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-28 18:11:33 -0500

    EvenHandler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import type {MouseEventHandler, } from 'react';
 import type { NextPage } from 'next';
 import Image from 'next/image'
 import { RandomFox } from '@/components/RandomFox';
@@ -7,21 +8,38 @@ const random = ():number => Math.floor(Math.random() * 123) + 1;
 const generateId = () => Math.random().toString(36).substring(2, 9);
 
 
-type ImageItem = {id: string, url: string};
+interface ImageItem  {id: string, url: string};
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItem>>([
-    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-  ]);
+  // const [images, setImages] = useState<Array<ImageItem>>([
+  //   {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+  //   {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+  //   {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+  //   {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
+  // ]);
+
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`
+    }
+
+    setImages([
+      ...images,
+      newImageItem
+    ]);
+  }
 
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">
-        Hello platzi!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello platzi!</h1>
+
+      <button onClick={addNewFox}>Add new Fox</button>
+
       {images.map((image)=>(
         <div key={image.id} className="p-4">
           <RandomFox image={image.url}/>
